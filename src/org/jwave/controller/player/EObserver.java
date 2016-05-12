a43fccecee9b66ffbba59d176c0dc766468adf33
Dario Cantarelli <dario.cantarelli@studio.unibo.it>
2016-05-12 09:23:40 +0200

    Modified ESource and EObserver interfaces.

--- src/org/jwave/controller/player/EObserver.java
+++ src/org/jwave/controller/player/EObserver.java
@@ -1,8 +1,8 @@
 package org.jwave.controller.player;
 
 //Taken from prof.Viroli's slides
-public interface EObserver<T> {
+public interface EObserver<T, Z> {
     
-    public void update(ESource<? extends T> s, T arg);
+    public void update(ESource<? extends T, ? extends Z> s, T arg);
     
 }
