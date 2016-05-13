e720a0845b48d5093030b0e55d0df5e68cd0f0a2
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2016-05-13 02:28:21 +0200

    Minor refactoring

--- src/org/jwave/view/FXGUI.java
+++ src/org/jwave/view/FXGUI.java
@@ -1,15 +1,9 @@
 package org.jwave.view;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import org.jwave.view.screens.FXMLScreens;
 
 import javafx.application.Application;
-import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
@@ -20,13 +14,7 @@ import javafx.stage.Stage;
  * @author Alessandro Martignano
  *
  */
-public class FXGUI extends Application implements UI, Initializable {   
- 
-    /**
-     * 
-     */
-    @FXML
-    private Button btnPlay;
+public class FXGUI extends Application implements UI {   
 
     /* (non-Javadoc)
      * @see javafx.application.Application#start(javafx.stage.Stage)
@@ -53,43 +41,4 @@ public class FXGUI extends Application implements UI, Initializable {
         launch(args);
     }   
     
-    /* (non-Javadoc)
-     * @see javafx.fxml.Initializable#initialize(java.net.URL, java.util.ResourceBundle)
-     */
-    @Override // This method is called by the FXMLLoader when initialization is complete
-    public void initialize(final URL location, final  ResourceBundle resources) {
-        assert btnPlay != null : "fx:id=\"btnPlay\" was not injected: check your FXML file 'Player.fxml'.";
-    }
-
-    /**
-     * 
-     */
-    @FXML
-    private void play() {
-        System.out.println("play");
-    }
-    
-    /**
-     * 
-     */
-    @FXML
-    private void stopPlay() {
-        System.out.println("stop");
-    }
-    
-    /**
-     * 
-     */
-    @FXML
-    private void next() {
-        System.out.println("next");
-    }
-    
-    /**
-     * 
-     */
-    @FXML
-    private void prev() {
-        System.out.println("prev");
-    }
 }
