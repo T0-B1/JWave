6c53edddb4ae486d2e07bf53c923e5fda3880a7b
Dario Cantarelli <dario.cantarelli@studio.unibo.it>
2016-05-19 17:39:19 +0200

    Corrected a problem in MetaDataImpl.

--- src/org/jwave/model/player/MetaDataImpl.java
+++ src/org/jwave/model/player/MetaDataImpl.java
@@ -3,15 +3,16 @@ package org.jwave.model.player;
 import java.io.Serializable;
 import java.lang.reflect.Method;
 import java.util.Arrays;
-import java.util.EnumMap;
+import java.util.HashMap;
 import java.util.List;
 import java.util.ListIterator;
 import java.util.Map;
 
+import org.jwave.controller.player.FileSystemHandler;
+
 import ddf.minim.AudioMetaData;
 import ddf.minim.AudioPlayer;
 import ddf.minim.Minim;
-import org.jwave.controller.player.FileSystemHandler;
 
 /**
  * This class is an implementation of {@link}MetaData.
@@ -32,7 +33,7 @@ public class MetaDataImpl implements MetaData, Serializable {
     private transient Minim minim;
     private transient AudioPlayer metaDataLoader;
     
-    private Map<MData, String> data;
+    private Map<String, String> data;
     
     //TODO check if deleting AudioPlayer after loading metadata is possible then enumType ecc.. is useless.
     
@@ -46,18 +47,18 @@ public class MetaDataImpl implements MetaData, Serializable {
         this.minim = new Minim(FileSystemHandler.getFileSystemHandler());
         this.metaDataLoader = this.minim.loadFile(absoluteFilePath);
         final AudioMetaData tmpData = this.metaDataLoader.getMetaData();
-        this.data = new EnumMap<>(MData.class);
-        final List<Method> m = Arrays.asList(AudioMetaData.class.getMethods()); 
-        final ListIterator<Method> mit = m.listIterator();
-        this.data.keySet().forEach(k -> {
-            if (mit.hasNext()) {
-                try {
-                    this.data.put(k, (String) mit.next().invoke(tmpData));
-                } catch (Exception e) {
-                    // TODO Auto-generated catch block
-                    e.printStackTrace();
-                }
-            } 
+        this.data = new HashMap<>();
+        final List<Method> methods = Arrays.asList(AudioMetaData.class.getMethods()); 
+        //loading metadata
+        methods.forEach(m -> {
+            String name = m.getName();
+            String value;
+            try {
+                value = (String) m.invoke(tmpData);
+                this.data.put(name, value);
+            } catch (Exception e) {
+                System.out.println("Problem while loading metadata");
+            }
         });
         this.metaDataLoader.close();
         this.minim.stop();
@@ -65,90 +66,90 @@ public class MetaDataImpl implements MetaData, Serializable {
     
     @Override
     public String getAlbum() {
-        return this.retrieve(MData.ALBUM);
+        return this.retrieve("album");
     }
 
     @Override
     public String getAuthor() {
-        return this.retrieve(MData.AUTHOR);
+        return this.retrieve("author");
     }
 
     @Override
     public String getComment() {
-        return this.retrieve(MData.COMMENT);
+        return this.retrieve("comment");
     }
 
     @Override
     public String getComposer() {
-        return this.retrieve(MData.COMPOSER);
+        return this.retrieve("composer");
     }
 
     @Override
     public String getCopyright() {
-        return this.retrieve(MData.COPYRIGHT);
+        return this.retrieve("copyright");
     }
 
     @Override
     public String getDate() {
-        return this.retrieve(MData.DATE);
+        return this.retrieve("date");
     }
 
     @Override
     public String getDisc() {
-        return this.retrieve(MData.DISC);
+        return this.retrieve("disc");
     }
 
     @Override
     public String getEncoded() {
-        return this.retrieve(MData.ENCODED);
+        return this.retrieve("encoded");
     }
 
     @Override
     public String getFileName() {
-        return this.retrieve(MData.FILENAME);
+        return this.retrieve("fileName");
     }
 
     @Override
     public String getGenre() {
-        return this.retrieve(MData.GENRE);
+        return this.retrieve("genre");
     }
 
     @Override
     public String getLength() {
-        return this.retrieve(MData.LENGTH);
+        return this.retrieve("length");
     }
 
     @Override
     public String getLyrics() {
-        return this.retrieve(MData.LYRICS);
+        return this.retrieve("lyrics");
     }
 
     @Override
     public String getOrchestra() {
-        return this.retrieve(MData.ORCHESTRA);
+        return this.retrieve("orchestra");
     }
 
     @Override
     public String getPublisher() {
-        return this.retrieve(MData.PUBLISHER);
+        return this.retrieve("publisher");
     }
 
     @Override
     public String getSampleFrameCount() {
-        return this.retrieve(MData.SAMPLE_FRAME_COUNT);
+        return this.retrieve("sampleFrameCount");
     }
 
     @Override
     public String getTitle() {
-        return this.retrieve(MData.TITLE);
+        return this.retrieve("title");
     }
 
     @Override
     public String getTrack() {
-        return this.retrieve(MData.TRACK);
+        return this.retrieve("track");
     }
     
-    private String retrieve(final MData key) {
+    private String retrieve(final String key) {
         return this.data.get(key);
     }
 }
