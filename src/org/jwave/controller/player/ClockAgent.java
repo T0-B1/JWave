372340a15a2af652a09a8e903b73f47693e57740
Dario Cantarelli <dario.cantarelli@studio.unibo.it>
2016-05-23 11:09:44 +0200

    Created ClockAgent.

---
+++ src/org/jwave/controller/player/ClockAgent.java
@@ -0,0 +1,63 @@
+package org.jwave.controller.player;
+
+import org.jwave.model.player.DynamicPlayer;
+
+public class ClockAgent implements Runnable {
+    
+    private Thread t;
+    private String name;
+    private DynamicPlayer dynPlayer;
+    private volatile boolean stopped;
+    
+    public ClockAgent(final DynamicPlayer player, final String threadName) {
+        this.dynPlayer = player;
+        this.stopped = false;
+        this.name = threadName;
+        this.t = new Thread(this, this.name);
+    }
+    
+    @Override
+    public void run() {
+        System.out.println("Running thread" + this.name);
+        while (!this.isStopped()) {
+            try {
+                this.checkInReproduction();
+                Thread.sleep(10L);
+            } catch (InterruptedException e) {
+                System.out.println("Thread interrupted");
+            }
+        }
+    }
+    
+    private void checkInReproduction() {
+        if (this.dynPlayer.getLoaded().isPresent() && !this.dynPlayer.isPlaying() && this.dynPlayer.hasStarted() && !this.dynPlayer.isPaused()) {
+            //loading next song
+            this.dynPlayer.play();
+        }
+    }
+    
+    /**
+     * Starts the clock agent thread.
+     */
+    public void startClockAgent() {
+        this.setStopped(false);
+        this.t.start();
+    }
+    
+    /**
+     * Stops the clock agent thread.
+     */
+    public void stopClockAgent() {
+        this.setStopped(true);
+    }
+    
+    private boolean isStopped() {
+        return this.stopped;
+    }
+    
+    private void setStopped(final boolean value) {
+        this.stopped = value;
+    }
+
+
+}
