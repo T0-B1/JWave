758a989558adf4068bc9e925b54a94962b4293eb
IndifferentAlex <indifferentalex@outlook.com>
2016-05-25 16:38:29 +0200

    Finished up Segment documentation to better describe intent

--- src/org/jwave/model/editor/Segment.java
+++ src/org/jwave/model/editor/Segment.java
@@ -1,5 +1,16 @@
 package org.jwave.model.editor;
 
+/**
+ * This interface models the concept of segments, of specific length, that
+ * subdivide any given cut of a song into multiple pieces. Segments have a
+ * beginning and an end that are relative to the length of the original,
+ * un-modified song.
+ * 
+ * Segments are similiar to cuts but are used for obtaining actual audio.
+ * 
+ * All "ms" acronyms stand for milliseconds.
+ *
+ */
 public interface Segment {
     /**
      * Gets the position where this segment begins.
