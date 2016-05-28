7e665dc2e88e9fb8722a42f692a828fbbd43489b
Alessandro Martignano <alessandr.martignano@studio.unibo.it>
2016-05-28 20:08:38 +0200

    Moved the starting point of controller and GUI in Main() in separated threads. Deleted JWaveApp. A new controller to be implemented soon.

--- src/org/jwave/controller/Main.java
+++ src/org/jwave/controller/Main.java
@@ -1,17 +1,35 @@
 package org.jwave.controller;
 
-/**
- * Starting point of the application.
- * @author Alessandro Martignano
- */
-public final class Main {
+import java.util.Stack;
+import org.jwave.view.FXEnvironment;
+import org.jwave.view.screens.PlayerScreenController;
+import com.sun.javafx.application.PlatformImpl;
+import javafx.application.Platform;
+import javafx.stage.Stage;
+
+public class Main {
     
-    private Main() { }
-    
-    /**
-     * @param args args
-     */
-    public static void main(final String[] args) {
-        new JWaveApp();
+    public static void main(String[] args) {
+        
+        PlatformImpl.startup(() -> { });
+        FXEnvironment environment = new FXEnvironment();
+        PlayerScreenController player = new PlayerScreenController(environment);
+        
+        Platform.runLater(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Stage primaryStage = new Stage();
+                    primaryStage.setTitle("JWave");
+                    environment.start(primaryStage);
+                } catch (Exception e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+                player.show();
+            }
+        });
+
     }
+
 }
