f7cc59e5e9950767328bca8d2e0727afe6461fb3
IndifferentAlex <indifferentalex@outlook.com>
2016-05-31 00:36:56 +0200

    Renamed SongDecorator to ModifiableSongDecorator to better convey purpose

--- src/org/jwave/model/editor/ModifiableSongDecorator.java
+++ src/org/jwave/model/editor/ModifiableSongDecorator.java
@@ -5,26 +5,29 @@ import java.util.UUID;
 import org.jwave.model.player.MetaDataRetriever;
 import org.jwave.model.player.Song;
 
-public abstract class SongDecorator implements Song {
+public abstract class ModifiableSongDecorator implements Song {
 	protected Song decoratedSong;
 	
-	public SongDecorator(Song decoratedSong) {
+	public ModifiableSongDecorator(Song decoratedSong) {
 		this.decoratedSong = decoratedSong;
 	}
 
+	@Override
 	public String getName() {
 		return decoratedSong.getName();
 	}
 
+	@Override
 	public String getAbsolutePath() {
 		return decoratedSong.getAbsolutePath();
 	}
 
+	@Override
 	public UUID getSongID() {
 		return decoratedSong.getSongID();
 	}
 	
-    @Override
+	@Override
     public MetaDataRetriever getMetaData() {
         return decoratedSong.getMetaData();
     }
